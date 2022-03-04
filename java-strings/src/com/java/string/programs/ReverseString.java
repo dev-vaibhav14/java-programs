2e22193442c29ee56607f4d4150098c5de97e09b
dev-vaibhav14 <vaibhavmahajan5@gmail.com>
2022-03-04 11:32:25 +0530

    added StringBuilder logic to reverse the string

--- java-strings/src/com/java/string/programs/ReverseString.java
+++ java-strings/src/com/java/string/programs/ReverseString.java
@@ -18,12 +18,19 @@ public class ReverseString {
 		System.out.println(str.charAt(4));
 		System.out.println(str.charAt(5));
 		System.out.println(str.charAt(6));
+		// normal for loop
+		/*
+		 * for (int i = str.length() -1 ; i >= 0 ; i--) {
+		 * System.out.print(str.charAt(i));
+		 * 
+		 * }
+		 */
 		
-		for (int i = str.length() -1 ; i >= 0  ; i--) 
-		{
-			System.out.print(str.charAt(i));
-			
-		}
+		// using StringBuilder
+		
+		StringBuilder sb = new StringBuilder();
+		sb.append(str);
+		System.out.println(sb.reverse());
 		
 
 	}
